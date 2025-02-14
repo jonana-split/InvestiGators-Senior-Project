be7a95bef9237fcd03083ec59f5b2ade0fd95d58
fbigabig <fbigabig@gmail.com>
2025-02-14 12:58:46 -0500

    Rigidbody player movement
    Fixed blurry sprites, changed compression to none and filter to point

--- indubio/Assets/player.cs
+++ indubio/Assets/player.cs
@@ -4,27 +4,33 @@ using UnityEngine.InputSystem;
 public class player : MonoBehaviour
 {
     private BoxCollider2D box;
-    private Vector3 moveDelta;
+    private Vector2 moveDelta;
     private InputAction move;
-    private float speed = .001f;
+    private Rigidbody2D rb;
+    [SerializeField] private float speed = 5f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         box = GetComponent<BoxCollider2D>();
         move = InputSystem.actions.FindAction("Move");
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         moveDelta = move.ReadValue<Vector2>();
-        if(moveDelta.x > 0)
+        Debug.Log(moveDelta);
+        if (moveDelta.x > 0)
         {
             transform.localScale = Vector3.one;
         }else if (moveDelta.x < 0)
         {
             transform.localScale = new Vector3(-1, 1, 1);
         }
-        transform.Translate(moveDelta * speed);
+        moveDelta *= speed;
+        Debug.Log(moveDelta);
+        rb.linearVelocity = moveDelta;
+        Debug.Log(rb.linearVelocity);
     }
 }
