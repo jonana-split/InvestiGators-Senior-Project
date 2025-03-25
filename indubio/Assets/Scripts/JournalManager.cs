a4f6db8c7130df5b3f9127f123928a89bae46d5d
Laura Schnettgoecke <schnett.ln@gmail.com>
2025-03-25 13:08:43 -0400

    Added multiple page functionality

--- indubio/Assets/Scripts/JournalManager.cs
+++ indubio/Assets/Scripts/JournalManager.cs
@@ -4,20 +4,52 @@ using System.IO;
 
 public class JournalManager : MonoBehaviour
 {
+    public GameObject backgroundPanel;
     public GameObject journalPanel;
     public GameObject journalButton;
     public TextMeshProUGUI journalText;
-    public string filePath = "Assets/TextFiles/sample.txt";
+    public string filePath;
     public TMP_InputField journalInputField;
+    public TextMeshProUGUI pageNumText;
+    public GameObject prevButton;
+    public GameObject nextButton;
+
+    private int pageNum;
 
     void Start()
     {
+        pageNum = 1;
         ReadTextFromFile();
+    }
+
+    void Update()
+    {
+        pageNumText.text = pageNum.ToString();
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            WriteTextToFile();
+        }
+
+        if (pageNum == 1)
+        {
+            prevButton.SetActive(false);
+        }
+        else if (pageNum == 10)
+        {
+            nextButton.SetActive(false);
+        }
+        else
+        {
+            prevButton.SetActive(true);
+            nextButton.SetActive(true);
+        }
     }
     public void OpenJournal()
     {
         journalPanel.SetActive(true);
         journalButton.SetActive(false);
+        backgroundPanel.SetActive(true);
         Time.timeScale = 0;
     }
 
@@ -25,11 +57,13 @@ public class JournalManager : MonoBehaviour
     {
         journalPanel.SetActive(false);
         journalButton.SetActive(true);
+        backgroundPanel.SetActive(false);
         Time.timeScale = 1;
     }
 
     public void ReadTextFromFile()
     {
+        filePath = "Assets/TextFiles/page" + pageNum + ".txt";
         if (File.Exists(filePath))
         {
             string fileContent = File.ReadAllText(filePath);
@@ -37,6 +71,10 @@ public class JournalManager : MonoBehaviour
         }
         else
         {
+            journalText.text = "This page has not been created yet";
+            //create a new txt file at the file path "Assets/TextFiles" called page[pageNum].txt
+            File.Create(filePath).Dispose();  // Automatically create the new file
+            journalText.text = "";
             Debug.Log("File not found");
         }
     }
@@ -45,6 +83,19 @@ public class JournalManager : MonoBehaviour
     {
         string textToWrite = journalInputField.text;
         File.AppendAllText(filePath, textToWrite + "\n");
+        journalInputField.text = "";
+        ReadTextFromFile();
+    }
+
+    public void SwitchPageUp()
+    {
+        pageNum++;
+        ReadTextFromFile();
+    }
+
+    public void SwitchPageDown()
+    {
+        pageNum--;
         ReadTextFromFile();
     }
 }
