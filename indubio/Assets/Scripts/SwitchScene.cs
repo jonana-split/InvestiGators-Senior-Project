b132045d721f90932ec9f47e200b841e953794f1
Laura Schnettgoecke <100947137+Lschnett13@users.noreply.github.com>
2025-04-12 21:22:58 -0400

    Added Credits Page

--- indubio/Assets/Scripts/SwitchScene.cs
+++ indubio/Assets/Scripts/SwitchScene.cs
@@ -5,10 +5,26 @@ using UnityEngine.SceneManagement;
 
 public class SwitchScene : MonoBehaviour
 {
-    public string SceneToLoad;
+    public string SceneToLoad1;
+    public string SceneToLoad2;
+    public bool credits;
+
+    public void SwitchSceneControl(bool creditsInput)
+    {
+        credits = creditsInput;
+    }
 
     public void SwitchTheScene()
     {
-        SceneManager.LoadScene(SceneToLoad);
+        if (credits)
+        {
+            Debug.Log("credits true");
+            SceneManager.LoadScene(SceneToLoad1);
+        }
+        else
+        {
+            Debug.Log("credits false");
+            SceneManager.LoadScene(SceneToLoad2);
+        }
     }
 }
