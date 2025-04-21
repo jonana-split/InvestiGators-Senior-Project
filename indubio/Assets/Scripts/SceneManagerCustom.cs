d17c8d69a6c728d9c56bd0616f7787ba8890a9a0
fbigabig <fbigabig@gmail.com>
2025-04-21 14:29:20 -0400

    Door Fix

--- indubio/Assets/Scripts/SceneManagerCustom.cs
+++ indubio/Assets/Scripts/SceneManagerCustom.cs
@@ -19,6 +19,8 @@ public class SceneManagerCustom : MonoBehaviour
         }
         else
         {
+            instance.sceneName = sceneName;
+            Debug.Log("Guh");
             Destroy(gameObject);
             return;
         }
